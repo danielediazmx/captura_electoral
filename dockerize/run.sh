72f6854b540ac4e7b84a250cb30d37fd8f597ba1
Daniel Diaz <danielediazmx@gmail.com>
2020-02-25 15:43:51 -0700

    something changed in database seeder and run sh

--- dockerize/run.sh
+++ dockerize/run.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 python manage.py makemigrations
 python manage.py migrate
-python manage.py loaddata dockerize/estado.json
-python manage.py loaddata dockerize/municipio.json
-python manage.py loaddata dockerize/localidad.json
-python manage.py loaddata dockerize/organismo.json
-python manage.py loaddata dockerize/seccion.json
-python manage.py loaddata dockerize/sector.json
-python manage.py loaddata dockerize/persona.json
+echo "Running seeder"
+python manage.py loaddata dockerize/seeder/estado.json
+python manage.py loaddata dockerize/seeder/municipio.json
+python manage.py loaddata dockerize/seeder/localidad.json
+python manage.py loaddata dockerize/seeder/organismo.json
+python manage.py loaddata dockerize/seeder/sector.json
+python manage.py loaddata dockerize/seeder/persona.json
+echo "Seeder completed"
 python manage.py runserver 0.0.0.0:8000
 python manage.py collectstatic
